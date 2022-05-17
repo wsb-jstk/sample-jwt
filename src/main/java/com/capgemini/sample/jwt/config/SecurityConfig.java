1d931c41591750d1f530112bbecaabda27451847
Maciej Lipski <maciej.lipski@capgemini.com>
2022-05-17 19:01:13 +0200

    Added inMemoryUsers

--- src/main/java/com/capgemini/sample/jwt/config/SecurityConfig.java
+++ src/main/java/com/capgemini/sample/jwt/config/SecurityConfig.java
@@ -1,9 +1,12 @@
 package com.capgemini.sample.jwt.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableMethodSecurity(securedEnabled = true)
@@ -24,6 +27,27 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.headers()
             .frameOptions()
             .sameOrigin(); // enables <frame> in H2 Console web UI
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        final PasswordEncoder passwordEncoder = passwordEncoder();
+        final String password = passwordEncoder.encode("password");
+        auth.inMemoryAuthentication()
+            .passwordEncoder(passwordEncoder)//
+            .withUser("user")
+            .password(password)
+            .roles("USER")//
+            .and()
+            .withUser("admin")
+            .password(password)
+            .roles("USER", "ADMIN");
+    }
+
+    // @Bean
+    PasswordEncoder passwordEncoder() {
+        // return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder(13);
     }
 
 }
